d76b0f88465ded344e55309e908542fe6cb9e04e
Doggo <dhernandez24e@hotmail.com>
2020-05-17 17:39:29 -0700

    working on inquirer inputs

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,9 @@
 const Employee = require('../lib/Employee');
 
+jest.mock('../lib/Employee');
+
 test('create an Employee object', () => {
-    const employee = new Employee('Jeff', 3, 'test@gmail.com');
+    const employee = new Employee();
 
     //name
     expect(employee.name).toEqual(expect.any(String));
@@ -15,8 +17,12 @@ test('create an Employee object', () => {
     //getRole 'Returns Employee'
 });
 
-// test('Gets name from employee item', () => {
-//     const employee = new Employee()
-//     //getName()
-//     expect(employee.getName()).toBe(String);
+test('Gets name from employee item', () => {
+    const employee = new Employee();
+    //getName()
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+});
+
+// test('Gets employee ID number', () => {
+//     const employee = new Employee();
 // })
